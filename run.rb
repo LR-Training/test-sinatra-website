5da7053e7a7de59ee2a707f6a0d03b5ff665134e
Andrew Moore <mooreandrewlr@gmail.com>
2014-11-05 07:52:31 +0000

    Improved Sample website

--- run.rb
+++ run.rb
@@ -1,7 +1,33 @@
 require 'sinatra'
+require 'erb'
 
 set :bind, '0.0.0.0'
 
 get '/' do
-  "Hello World!"
+  erb :index
+end
+
+post '/login' do
+  if ((params[:username] == 'user@example.org') && (params[:password] = 'password'))
+    redirect '/menu'
+  elsif (params[:username] == 'blocked@example.org')
+    redirect '/blocked'
+  else
+    redirect '/unknown'
+  end
+end
+
+get '/unknown' do
+  @message = 'Unknown User'
+  erb :message
+end
+
+get '/blocked' do
+  @message = 'User Blocked'
+  erb :message
+end
+
+get '/menu' do
+  @message = 'Logged in'
+  erb :message
 end
