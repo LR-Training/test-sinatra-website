9871b7f0230c70a999d04841d39fcfd8c3d1bb49
lrgeofflewis <LRGeoff@LRWorkComputer.local>
2014-12-05 15:26:58 +0000

    added menu and form with no css

--- run.rb
+++ run.rb
@@ -17,6 +17,24 @@ post '/login' do
   end
 end
 
+post '/form-submit' do
+
+  @form_details = Hash.new()
+  @form_details['forename'] = params[:forename]
+  @form_details['surname'] = params[:surname]
+  @form_details['house_number'] = params[:house_number]
+  @form_details['street'] = params[:street]
+  @form_details['city'] = params[:city]
+  @form_details['postcode'] = params[:postcode]
+  @form_details['sex'] = params[:sex]
+  @form_details['interest'] = Hash.new()
+  @form_details['interest']['walking'] = params[:walking]
+  @form_details['interest']['cycling'] = params[:cycling]
+  @form_details['interest']['swimming'] = params[:swimming]
+  @form_details['cars'] = params[:cars]
+  erb :form_output
+end
+
 get '/unknown' do
   @message = 'Unknown User'
   erb :message
@@ -29,5 +47,9 @@ end
 
 get '/menu' do
   @message = 'Logged in'
-  erb :message
+  erb :menu
+end
+
+get '/form-details' do
+  erb :form
 end
