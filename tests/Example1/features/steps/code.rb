f55cfb53437ed293f67d4f340421c92f09ddaa9f
Andrew Moore <mooreandrewlr@gmail.com>
2015-04-28 12:24:17 +0100

    Added Sample Test

---
+++ tests/Example1/features/steps/code.rb
@@ -0,0 +1,27 @@
+When(/^I create a user$/) do
+
+  data = {}
+  data['user_name'] = 'User' + Time.new().to_i.to_s; # Create a random user name
+  data['user_password'] = 'password';
+
+  result = rest_post_call('http://localhost:4567/add_user', data);
+  $json_result = JSON.parse(result.body)
+end
+
+Then(/^the user is successfully created$/) do
+  expect($json_result['status']).to eq('User Added')
+end
+
+When(/^I create a user which already exists$/) do
+  data = {}
+  data['user_name'] = 'User' + Time.new().to_i.to_s; # Create a random user name
+  data['user_password'] = 'password';
+
+  result = rest_post_call('http://localhost:4567/add_user', data);
+  result = rest_post_call('http://localhost:4567/add_user', data); # Add the user a second time
+  $json_result = JSON.parse(result.body)
+end
+
+Then(/^the user is not created$/) do
+  expect($json_result['status']).to eq('User Already Exists')
+end
---
+++ tests/Example1/features/test.feature
@@ -0,0 +1,9 @@
+Feature: Create User to test site
+
+Scenario: Create new user that doesn't exist
+When I create a user
+Then the user is successfully created
+
+Scenario: Create new user that already exists
+When I create a user which already exists
+Then the user is not created
